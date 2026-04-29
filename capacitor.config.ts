bde705063e77acd01f5b479af66c21aaaceb45ef
ocaa30 <if24.rosaliaindah@mhs.ubpkarawang.ac.id>
2026-04-29 14:38:47 +0700

    Update project structure dan konfigurasi Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.InnerFlow.app',
+  appId: 'com.rosalia.InnerFlow',
   appName: 'InnerFlow',
   webDir: 'www'
 };
