f8903d786504455204f0b0840728fdb7295641e6
ocaa30 <if24.rosaliaindah@mhs.ubpkarawang.ac.id>
2026-04-29 14:53:10 +0700

    update konfigurasi capacitor dan perbaikan fitur

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.rosalia.InnerFlow',
+  appId: 'com.innerflow.app.kesehatan',
   appName: 'InnerFlow',
   webDir: 'www'
 };
